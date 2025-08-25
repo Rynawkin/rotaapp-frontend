87f1d8de39668182d6a5297fbd484bfcfb7adab5
Bakırcılar <ensarucarer@bakircilargrup.com>
2025-08-25 14:46:17 +0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ export const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 30000,
+  timeout: 60000,
 });
 
 // Sayfa yüklendiğinde token varsa header'a ekle
